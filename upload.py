9628fdc18284079a4256e8ff09c8738d1021baa4
David Wolf <68775926+devidw@users.noreply.github.com>
2024-07-10 19:03:18 +0200

    my

--- upload.py
+++ upload.py
@@ -5,4 +5,4 @@ storage = dropbox.Dropbox(oauth2_access_token=os.environ.get("DROPBOX_API_KEY"))
 
 with open("./output.mp4", "rb") as f:
     vid = f.read()
-    storage.files_upload(vid, "/daily.mp4")
+    storage.files_upload(vid, "/daily.mp4", mode=dropbox.files.WriteMode.overwrite)
